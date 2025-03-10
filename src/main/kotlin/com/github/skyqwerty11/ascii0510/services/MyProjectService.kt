f3d0b39f95e008a4897f26ad1728629d6b69f3c6
GitHub Action <action@github.com>
2025-03-10 17:30:28 +0000

    Template cleanup

--- src/main/kotlin/com/github/skyqwerty11/ascii0510/services/MyProjectService.kt
+++ src/main/kotlin/com/github/skyqwerty11/ascii0510/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.skyqwerty11.ascii0510.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.skyqwerty11.ascii0510.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
